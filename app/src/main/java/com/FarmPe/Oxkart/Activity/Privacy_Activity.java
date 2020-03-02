b9c839f7f825afa226be273546b078322f90af50
vinod <vinod.s@renewin.com>
2020-03-02 17:14:40 +0530

    model

--- app/src/main/java/com/FarmPe/Oxkart/Activity/Privacy_Activity.java
+++ app/src/main/java/com/FarmPe/Oxkart/Activity/Privacy_Activity.java
@@ -2,29 +2,25 @@ package com.FarmPe.Oxkart.Activity;
 
 
 
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.FarmPe.Oxkart.Bean.FarmsImageBean;
+import com.FarmPe.Oxkart.Fragment.PrivacyPolicyFragment;
 import com.FarmPe.Oxkart.Fragment.Verification_Last_Fragment;
 import com.FarmPe.Oxkart.R;
 import com.FarmPe.Oxkart.SessionManager;
-import com.FarmPe.Oxkart.Urls;
-import com.FarmPe.Oxkart.Volly_class.Crop_Post;
-import com.FarmPe.Oxkart.Volly_class.VoleyJsonObjectCallback;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.ArrayList;
@@ -32,15 +28,21 @@ import java.util.List;
 
 
 public class Privacy_Activity extends AppCompatActivity implements ConnectivityReceiver.ConnectivityReceiverListener{
+
+
     public static List<FarmsImageBean> newOrderBeansList = new ArrayList<>();
+
     public static RecyclerView recyclerView;
+
     TextView toolbar_title;
     Fragment selectedFragment;
     String toast_internet,toast_nointernet;
     JSONObject lngObject;
     SessionManager sessionManager;
     LinearLayout back_feed,main_layout;
-    JSONObject verify_status;
+
+
+
     public static boolean connectivity_check;
     ConnectivityReceiver connectivityReceiver;
 
@@ -58,32 +60,52 @@ public class Privacy_Activity extends AppCompatActivity implements ConnectivityR
 
 
     private void showSnack(boolean isConnected) {
+        String message = null;
         int color=0;
         if (isConnected) {
             if(connectivity_check) {
-                Toast toast = Toast.makeText(Privacy_Activity.this,toast_internet, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP|Gravity.CENTER,0,0);
-                toast.show();
+                message = "Good! Connected to Internet";
+                color = Color.WHITE;
+
+                int duration=1000;
+                Snackbar snackbar = Snackbar.make(main_layout,toast_internet, duration);
+                View sbView = snackbar.getView();
+                TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+                textView.setBackgroundColor(ContextCompat.getColor(Privacy_Activity.this,R.color.orange));
+                textView.setTextColor(Color.WHITE);
+                snackbar.show();
+
+
                 connectivity_check=false;
             }
 
         } else {
+            message = "No Internet Connection";
+            color = Color.RED;
+
+            int duration=1000;
             connectivity_check=true;
-            Toast toast = Toast.makeText(Privacy_Activity.this,toast_nointernet, Toast.LENGTH_SHORT);
-            toast.setGravity(Gravity.TOP|Gravity.CENTER,0,0);
-            toast.show();
+
+            Snackbar.make(findViewById(android.R.id.content),toast_nointernet, duration).show();
+
+
         }
     }
 
     @Override
     public void onResume() {
         super.onResume();
+
         final IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
         connectivityReceiver = new ConnectivityReceiver();
         registerReceiver(connectivityReceiver, intentFilter);
         MyApplication.getInstance().setConnectivityListener(this);
+
+
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,48 +113,42 @@ public class Privacy_Activity extends AppCompatActivity implements ConnectivityR
         setContentView(R.layout.test);
         checkConnection();
         sessionManager = new SessionManager(this);
+
         back_feed=findViewById(R.id.back_feed);
         main_layout=findViewById(R.id.main_layout);
         toolbar_title=findViewById(R.id.toolbar_title);
-;       try {
+        ;
+
+
+        //  profile_view.setVisibility(View.GONE);
+
+
+
+        try {
             lngObject = new JSONObject(sessionManager.getRegId("language"));
+
+
+
             toast_internet = lngObject.getString("GoodConnectedtoInternet");
             toast_nointernet = lngObject.getString("NoInternetConnection");
+
+
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        try{
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("UserId",sessionManager.getRegId("userId"));
-            Crop_Post.crop_posting(this, Urls.Get_Verification_Status, jsonObject, new VoleyJsonObjectCallback() {
-                @Override
-                public void onSuccessResponse(JSONObject result) {
 
-                    try{
-                        verify_status = result.getJSONObject("VerificationStatus");
-                        Boolean user_uploaded = verify_status.getBoolean("IsUserUploaded");
-                        Bundle bundle=new Bundle();
-                            selectedFragment = Verification_Last_Fragment.newInstance();
-                            bundle.putBoolean("VER_SATTUS",user_uploaded);
-                            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                            transaction.replace(R.id.frame_layout, selectedFragment);
-                            selectedFragment.setArguments(bundle);
-                            transaction.commit();
 
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
+        selectedFragment = Verification_Last_Fragment.newInstance();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, selectedFragment);
+        transaction.commit();
 
-                }
-            });
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void onNetworkConnectionChanged(boolean isConnected) {
         showSnack(isConnected);
+
     }
 }
--- app/src/main/java/com/FarmPe/Oxkart/Fragment/Verification_Last_Fragment.java
+++ app/src/main/java/com/FarmPe/Oxkart/Fragment/Verification_Last_Fragment.java
@@ -1,7 +1,5 @@
 package com.FarmPe.Oxkart.Fragment;
 
-
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -19,166 +17,245 @@ import android.widget.TextView;
 import com.FarmPe.Oxkart.Activity.HomePage_With_Bottom_Navigation;
 import com.FarmPe.Oxkart.Activity.New_Login_Activity2;
 import com.FarmPe.Oxkart.Activity.New_OTP_Page_Activity;
-import com.FarmPe.Oxkart.Activity.Privacy_Activity;
 import com.FarmPe.Oxkart.Activity.Verification_Activity;
 import com.FarmPe.Oxkart.R;
 import com.FarmPe.Oxkart.SessionManager;
 import com.FarmPe.Oxkart.Urls;
 import com.FarmPe.Oxkart.Volly_class.Crop_Post;
 import com.FarmPe.Oxkart.Volly_class.VoleyJsonObjectCallback;
-import com.google.gson.JsonArray;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
+
+
 public class Verification_Last_Fragment extends Fragment {
+    Boolean user_uploaded;
     Fragment selectedFragment;
-   public static  LinearLayout linear_layout,cont_btn;
-   boolean doubleBackToExitPressedOnce = false;
-   JSONObject verify_status;
-   TextView toolbar_title,mobile_no,proceed_txt,in_progress_details,success_details;
-   public static  TextView user_status,ph_no,user_status_text;
-   ImageView in_progress_image,success_image;
-   SessionManager sessionManager;
+    public static  LinearLayout linear_layout,cont_btn;
+    boolean doubleBackToExitPressedOnce = false;
+    JSONObject verify_status;
+    TextView toolbar_title,mobile_no,proceed_txt,in_progress_details,success_details;
+    public static  TextView user_status,ph_no,user_status_text;
+    ImageView in_progress_image,success_image;
+    SessionManager sessionManager;
     public static JSONObject lngObject;
-   JSONArray get_location_array;
-   boolean ver_status;
 
-   public static Verification_Last_Fragment newInstance() {
-       Verification_Last_Fragment fragment = new Verification_Last_Fragment();
-       return fragment;
-   }
-   @Override
-   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-       View view = inflater.inflate(R.layout.verify_last_layout, container, false);
-       linear_layout = view.findViewById(R.id.linear_layout);
-       user_status = view.findViewById(R.id.user_status);
-       ph_no = view.findViewById(R.id.ph_no);
-       cont_btn = view.findViewById(R.id.cont_btn);
-       toolbar_title = view.findViewById(R.id.setting_tittle);
-       mobile_no = view.findViewById(R.id.mobile_no);
-       user_status_text = view.findViewById(R.id.user_status_text);
-       proceed_txt = view.findViewById(R.id.proceed_txt);
-       in_progress_details = view.findViewById(R.id.in_progress_details);
-       success_details = view.findViewById(R.id.success_details);
-       in_progress_image = view.findViewById(R.id.in_progress_image);
-       success_image = view.findViewById(R.id.success_image);
-       sessionManager = new SessionManager(getActivity());
-       ph_no.setText(sessionManager.getRegId("phone"));
-       System.out.println("dhfgfjh" + sessionManager.getRegId("phone"));
-       ver_status=getArguments().getBoolean("VER_SATTUS");
-       cont_btn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               if(ver_status){
 
-                   Intent intent = new Intent(getActivity(),HomePage_With_Bottom_Navigation.class);
-                   sessionManager.savelocation(sessionManager.getRegId("location"));
-                   startActivity(intent);
-               }else{
-                   Bundle bundle=new Bundle();
-                   selectedFragment = Verification_Last_Fragment.newInstance();
-                   bundle.putBoolean("VER_SATTUS",ver_status);
-                   FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-                   transaction.replace(R.id.frame_layout, selectedFragment);
-                   selectedFragment.setArguments(bundle);
-                   transaction.commit();
-               }
-           }
-       });
-       try {
-           lngObject = new JSONObject(sessionManager.getRegId("language"));
-           toolbar_title.setText(lngObject.getString("Verification"));
-           mobile_no.setText(lngObject.getString("PhoneNumber"));
-           user_status_text.setText(lngObject.getString("Status"));
-           in_progress_details.setText(lngObject.getString("OuragentwillcallyousoontoverifyDetailsPleasebeavailableovercall").replace("\n",""));
-           success_details.setText(lngObject.getString("YourverificationissuccessfulClickproceedtostartexploring"));
-           proceed_txt.setText(lngObject.getString("PROCEED").replace("\n",""));
-           if(ver_status){
-               user_status.setText("In Progress");
-               in_progress_details.setVisibility(View.VISIBLE);
-               in_progress_image.setVisibility(View.VISIBLE);
-               user_status.setText(lngObject.getString("InProgress").replace("\n",""));
 
-           }else{
-               user_status.setText("In Progress");
-               in_progress_details.setVisibility(View.VISIBLE);
-               in_progress_image.setVisibility(View.VISIBLE);
-               user_status.setText(lngObject.getString("InProgress").replace("\n",""));
-           }
+    public static Verification_Last_Fragment newInstance() {
+        Verification_Last_Fragment fragment = new Verification_Last_Fragment();
+        return fragment;
 
-       } catch (JSONException e) {
-           e.printStackTrace();
-       }
-       view.setFocusableInTouchMode(true);
-       view.requestFocus();
-       view.setOnKeyListener(new View.OnKeyListener() {
-           @Override
-           public boolean onKey(View v, int keyCode, KeyEvent event) {
+    }
 
-               if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
 
-                   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                       // this.finishAffinity();
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.verify_last_layout, container, false);
 
-                       if (doubleBackToExitPressedOnce) {
-                           Intent intent = new Intent(Intent.ACTION_MAIN);
-                           intent.addCategory(Intent.CATEGORY_HOME);
-                           intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
-                           startActivity(intent);
-                           getActivity().finish();
-                           System.exit(0);
-                       }
+        //   Status_bar_change_singleton.getInstance().color_change(getActivity());
 
-                       doubleBackToExitPressedOnce = true;
+        linear_layout = view.findViewById(R.id.linear_layout);
+        user_status = view.findViewById(R.id.user_status);
+        ph_no = view.findViewById(R.id.ph_no);
+        cont_btn = view.findViewById(R.id.cont_btn);
+        toolbar_title = view.findViewById(R.id.setting_tittle);
+        mobile_no = view.findViewById(R.id.mobile_no);
+        user_status_text = view.findViewById(R.id.user_status_text);
+        proceed_txt = view.findViewById(R.id.proceed_txt);
+        in_progress_details = view.findViewById(R.id.in_progress_details);
+        success_details = view.findViewById(R.id.success_details);
+        in_progress_image = view.findViewById(R.id.in_progress_image);
+        success_image = view.findViewById(R.id.success_image);
 
-                       new Handler().postDelayed(new Runnable() {
-                           @Override
-                           public void run() {
-                               doubleBackToExitPressedOnce = false;
-                           }
-                       }, 3000);
-                   }
+        sessionManager = new SessionManager(getActivity());
 
-                   return true;
-               }
-               return false;
-           }
-       });
+        ph_no.setText(sessionManager.getRegId("phone"));
+        System.out.println("dhfgfjh" + sessionManager.getRegId("phone"));
 
-       locationcaptured();
-       return view;
-   }
 
-   public void locationcaptured(){
-       try {
 
-           JSONObject jsonObject = new JSONObject();
-           jsonObject.put("UserId", sessionManager.getRegId("userId"));
 
-           Crop_Post.crop_posting(getActivity(), Urls.Get_Shop_Location, jsonObject, new VoleyJsonObjectCallback() {
-               @Override
-               public void onSuccessResponse(JSONObject result) {
-                   try {
-                       get_location_array = result.getJSONArray("clocationList");
-                       for (int i = 0; i < get_location_array.length(); i++) {
-                           JSONObject jsonObject1 = get_location_array.getJSONObject(i);
-                           String location_captured_image = jsonObject1.getString("CapturedLocation");
-                           sessionManager.savelocation(location_captured_image);
-                       }
-                   } catch (Exception e) {
-                       e.printStackTrace();
-                   }
+        try {
 
-               }
-           });
+
+            lngObject = new JSONObject(sessionManager.getRegId("language"));
+
+            System.out.println("llllllllllllkkkkkkkkkkkkkkk" + lngObject.getString("EnterPhoneNo"));
+
+            toolbar_title.setText(lngObject.getString("Verification"));
+            mobile_no.setText(lngObject.getString("PhoneNumber"));
+            user_status_text.setText(lngObject.getString("Status"));
+
+            in_progress_details.setText(lngObject.getString("OuragentwillcallyousoontoverifyDetailsPleasebeavailableovercall").replace("\n",""));
+            success_details.setText(lngObject.getString("YourverificationissuccessfulClickproceedtostartexploring"));
+
+
+            proceed_txt.setText(lngObject.getString("PROCEED").replace("\n",""));
+
+
+
+            //  pass.setHint(lngObject.getString("Password"));
+            //  remember_me.setText(lngObject.getString("RememberMe"));
+
+
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+        view.setOnKeyListener(new View.OnKeyListener() {
+
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        // this.finishAffinity();
+
+                        if (doubleBackToExitPressedOnce) {
+                            Intent intent = new Intent(Intent.ACTION_MAIN);
+                            intent.addCategory(Intent.CATEGORY_HOME);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
+                            startActivity(intent);
+                            getActivity().finish();
+                            System.exit(0);
+                        }
+
+                        doubleBackToExitPressedOnce = true;
+                        // Toast.makeText(getActivity().getApplicationContext(), "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+//                       int duration = 1000;
 //
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
+//                       Snackbar snackbar = Snackbar
+//                               .make(linear_layout,"Please Click Back To Exit", duration);
+//                       View snackbarView = snackbar.getView();
+//                       TextView tv = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+//                       tv.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.orange));
+//                       tv.setTextColor(Color.WHITE);
+//
+//
+//                       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                           tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+//                       } else {
+//                           tv.setGravity(Gravity.CENTER_HORIZONTAL);
+//                       }
+//                       snackbar.show();
 
-   }
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                doubleBackToExitPressedOnce = false;
+                            }
+                        }, 3000);
+                    }
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+        cont_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user_uploaded.equals(false)){
+                    Intent intent = new Intent(getActivity(), Verification_Activity.class);
+                    startActivity(intent);
+                }else {
+                    Intent intent = new Intent(getActivity(),HomePage_With_Bottom_Navigation.class);
+                    startActivity(intent);
+                }
+
+
+
+            }
+        });
+
+
+        try{
+
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("UserId",sessionManager.getRegId("userId"));
+            System.out.println("deyuiirwe" + sessionManager.getRegId("userId"));
+
+            Crop_Post.crop_posting(getActivity(), Urls.Get_Verification_Status, jsonObject, new VoleyJsonObjectCallback() {
+                @Override
+                public void onSuccessResponse(JSONObject result) {
+                    System.out.println("ghdgfd" + result);
+
+                    try{
+
+                        verify_status = result.getJSONObject("VerificationStatus");
+
+                        user_uploaded = verify_status.getBoolean("IsUserUploaded");
+
+                        if(user_uploaded.equals(false)){
+
+                            user_status.setText("In Progress");
+                            in_progress_details.setVisibility(View.VISIBLE);
+
+                            user_status.setText(lngObject.getString("InProgress").replace("\n",""));
+                            in_progress_image.setVisibility(View.VISIBLE);
+
+
+
+                        }else{
+
+                            user_status.setText("Successfull");
+
+                            user_status.setText(lngObject.getString("Successful"));
+
+                            success_details.setVisibility(View.VISIBLE);
+                            success_image.setVisibility(View.VISIBLE);
+
+
+
+                        }
+
+
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+//        Continue.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                selectedFragment = VerificationRejectedFragment.newInstance();
+//                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+//                transaction.replace(R.id.frame_layout1, selectedFragment);
+//                transaction.addToBackStack("verification");
+//                transaction.commit();
+//
+//            }
+//        });
+
+
+
+
+
+
+
+        return view;
+    }
 }
 
